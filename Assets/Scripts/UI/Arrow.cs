219cf280a81d74071f5b40dfa0a3be45e1375c3a
Hung Nguyen Huy <hungnh1812@gmail.com>
2020-03-25 23:45:07 +0700

    - UI: Show All Arrow in box

---
+++ Assets/Scripts/UI/Arrow.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    public enum ArrowDirection
+    {
+        NODE = -1,
+        LEFT,
+        UP,
+        RIGHT,
+        DOWN,
+    };
+
+    public ArrowDirection direction = ArrowDirection.NODE;
+
+    private Animator animator;
+
+    public void SetSuccessfulArrow(bool successful)
+    {
+        if (successful)
+            animator.SetTrigger("Successful");
+        else
+            animator.SetTrigger("Failure");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+}
---
+++ Assets/Scripts/UI/KeyController.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class KeyController : MonoBehaviour
+{
+    public enum SessionResult
+    {
+        PERFECT,
+        GREAT,
+        GOOD,
+        FAIL,
+        MISS
+    };
+
+    public static KeyController instance;
+
+    public GameObject arrowHolder;
+    public GameObject arrowPrefab;
+    public int goodPercentTime = 100;
+    public int greatPercentTime = 80;
+    public int perfectPercentTime = 50;
+
+    private List<Arrow> arrows;
+    private float keySessionTimeout;
+    private int keyIndex = 0;
+    private bool canTrackKey = false;
+    private float currentTime = 0;
+
+    public void BuildKeyList(int numberOfKey, float keySessionTimeout)
+    {
+        this.currentTime = 0.0f;
+        this.canTrackKey = false;
+        this.keyIndex = 0;
+        this.keySessionTimeout = keySessionTimeout;
+
+        foreach(Arrow arrow in arrows)
+        {
+            arrow.gameObject.Kill();
+        }
+        arrows.Clear();
+
+        SpriteRenderer sr = this.GetComponent<SpriteRenderer>();
+        sr.size = new Vector2((arrowPrefab.GetComponent<RectTransform>().sizeDelta.x + 5) * numberOfKey, sr.size.y);
+        this.GetComponent<SpriteRenderer>().size = sr.size;
+
+        RectTransform rt = this.GetComponent<RectTransform>();
+        rt.sizeDelta = new Vector2(sr.size.x, rt.sizeDelta.y);
+        this.GetComponent<RectTransform>().sizeDelta = rt.sizeDelta;
+
+        for (int i=0; i<numberOfKey; i++)
+        {
+            Arrow.ArrowDirection direction = (Arrow.ArrowDirection)Random.Range(0, 3);
+            Vector3 rotationEule = new Vector3(0, 0, 90 * (int)direction);
+            Arrow arrow = arrowPrefab.Spawn(Vector3.zero, Quaternion.Euler(rotationEule), Vector3.one, arrowHolder.transform, true, true)
+                                    .GetComponent<Arrow>();
+            arrow.direction = direction;
+            arrows.Add(arrow);
+        }
+    }
+
+    void StartTrackKey()
+    {
+        canTrackKey = true;
+    }
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+        KeyController.instance = this;
+        arrows = new List<Arrow>();
+    }
+
+    private void DispatchSesstionResult(SessionResult result)
+    {
+        canTrackKey = false;
+        Debug.Log(result.ToString());
+    }
+
+    private void PerformArrowDown(int index, Arrow.ArrowDirection direction)
+    {
+        Arrow arrow = arrows[index];
+        bool successful = arrow.direction == direction;
+        arrow.SetSuccessfulArrow(successful);
+        if (!successful)
+            DispatchSesstionResult(SessionResult.FAIL);
+        else
+        {
+            if (keyIndex == arrows.Count -1)
+            {
+                float percent = currentTime / keySessionTimeout * 100;
+                if (percent <= perfectPercentTime)
+                {
+                    DispatchSesstionResult(SessionResult.PERFECT);
+                }
+                else if (percent <= greatPercentTime)
+                {
+                    DispatchSesstionResult(SessionResult.GREAT);
+                }
+                else if (percent <= goodPercentTime)
+                {
+                    DispatchSesstionResult(SessionResult.GOOD);
+                }
+            }
+        }
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canTrackKey)
+        {
+            currentTime = currentTime + Time.deltaTime;
+
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                PerformArrowDown(keyIndex, Arrow.ArrowDirection.LEFT);
+            }
+            else if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                PerformArrowDown(keyIndex, Arrow.ArrowDirection.UP);
+            }
+            else if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                PerformArrowDown(keyIndex, Arrow.ArrowDirection.RIGHT);
+            }
+            else if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                PerformArrowDown(keyIndex, Arrow.ArrowDirection.DOWN);
+            }
+
+            if (currentTime > keySessionTimeout)
+            {
+                DispatchSesstionResult(SessionResult.MISS);
+            }
+        }
+    }
+}
