f2cefab323195ee2e4389011d7f32dfa985ac8b3
thaihoang1406 <nthoang1406@gmail.com>
2020-03-26 11:00:28 +0700

    +Add player to scene

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject Player1;
+    public GameObject Player2;
+    public GameObject Player3;
+
+    void Start()
+    {
+        InitPlayer();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void InitPlayer()
+    {
+
+        GameObject temp = Instantiate(Player1, new Vector3(-3, 0, 0),Quaternion.identity);
+        temp.transform.eulerAngles = new Vector3(
+            temp.transform.eulerAngles.x,
+            temp.transform.eulerAngles.y + 180,
+            temp.transform.eulerAngles.z);
+
+        temp = Instantiate(Player2, new Vector3(3, 0, 0), Quaternion.identity);
+        temp.transform.eulerAngles = new Vector3(
+            temp.transform.eulerAngles.x,
+            temp.transform.eulerAngles.y + 180,
+            temp.transform.eulerAngles.z);
+
+        temp = Instantiate(Player3, new Vector3(0, 0, -4), Quaternion.identity);
+        temp.transform.eulerAngles = new Vector3(
+            temp.transform.eulerAngles.x,
+            temp.transform.eulerAngles.y + 180,
+            temp.transform.eulerAngles.z);
+    }
+}
