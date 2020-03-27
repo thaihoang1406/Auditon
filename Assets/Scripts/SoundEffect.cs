81f98f3382fa067b3156d2cd5e8e8fa764796d31
little_lion <tuduchoang@gmail.com>
2020-03-27 11:30:41 +0700

    + implement sound

---
+++ Assets/Scripts/SoundEffect.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class SoundEffect : MonoBehaviour
+{
+    public AudioClip[] m_clips;
+
+    SoundManager m_sm;
+
+    private void Start() { m_sm = SoundManager.instance; }
+
+    public void Play()
+    {
+        var clip = m_clips[Random.Range(0, m_clips.Length)];
+        m_sm.PlaySingle(clip);
+    }
+}
