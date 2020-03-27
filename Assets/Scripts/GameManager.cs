8fb97d3df0c0b0af7fec4abde14ca21231adce4c
thaihoang1406 <nthoang1406@gmail.com>
2020-03-27 09:31:30 +0700

    +Add miss particle and add dance to hierachy

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,9 +5,6 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    public GameObject Player1Prefab;
-    public GameObject Player2Prefab;
-    public GameObject Player3Prefab;
 
     public Text Player1Score;
     public Text Player2Score;
@@ -16,12 +13,13 @@ public class GameManager : MonoBehaviour
     public GameObject PerfectEffectPrefab;
     public GameObject GreatEffectPrefab;
     public GameObject GoodEffectPrefab;
+    public GameObject MissEffectPrefab;
 
     public GameObject ResultPopup;
 
-    GameObject Player1;
-    GameObject Player2;
-    GameObject Player3;
+    public GameObject Player1;
+    public GameObject Player2;
+    public GameObject Player3;
 
     int score1;
     int score2;
@@ -35,7 +33,7 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         numRound = 0;
-        InitPlayer();
+        //InitPlayer();
         score1 = score2 = score3 = 0;
         ArrowController.instance.BuildArrowList(5, 8);
         ArrowController.instance.StartTrackKey();
@@ -64,13 +62,13 @@ public class GameManager : MonoBehaviour
             int rand = Random.Range(1, 6);
             triggerDance = "Dancing" + rand.ToString();
         }
-        int rand1 = Random.Range(0, 2);
+        int rand1 = Random.Range(0, 3);
         score2 += getScore(rand1);
         Player2Score.text = "Player 2: " + score2;
         Player2.GetComponent<Animator>().SetTrigger(triggerDance);
         initEffect(rand1, Player2.transform.GetChild(6).transform.position);
 
-        int rand2 = Random.Range(0, 2);
+        int rand2 = Random.Range(0, 3);
         score3 += getScore(rand2);
         Player3Score.text = "Player 3: " + score3;
         Player3.GetComponent<Animator>().SetTrigger(triggerDance);
@@ -87,6 +85,8 @@ public class GameManager : MonoBehaviour
                 return 80;
             case 2:
                 return 50;
+            case 3:
+                return 0;
         }
         return 0;
     }
@@ -103,6 +103,9 @@ public class GameManager : MonoBehaviour
                 break;
             case 2:
                 Instantiate(GoodEffectPrefab, pos, Quaternion.identity);
+                break;
+            case 3:
+                Instantiate(MissEffectPrefab, pos, Quaternion.identity);
                 break;
 
         }
@@ -137,15 +140,15 @@ public class GameManager : MonoBehaviour
                 switch (sessionResult)
                 {
                     case ArrowController.SessionResult.PERFECT:
-                        score1 += getScore(0)*3;
+                        score1 += getScore(0);
                         initEffect(0, Player1.transform.GetChild(3).transform.position);
                         break;
                     case ArrowController.SessionResult.GREAT:
-                        score1 += getScore(1)*3;
+                        score1 += getScore(1);
                         initEffect(1, Player1.transform.GetChild(3).transform.position);
                         break;
                     case ArrowController.SessionResult.GOOD:
-                        score1 += getScore(2)*3;
+                        score1 += getScore(2);
                         initEffect(2, Player1.transform.GetChild(3).transform.position);
                         break;
 
@@ -153,6 +156,8 @@ public class GameManager : MonoBehaviour
 
                 Player1Score.text = "Player 1: " + score1;
             }
+            else
+                initEffect(3, Player1.transform.GetChild(3).transform.position);
             string temp = "Ultimate";
             ProcessDanceNPC(temp);
         }
@@ -167,6 +172,8 @@ public class GameManager : MonoBehaviour
                 Player1.GetComponent<Animator>().SetTrigger(temp);
 
             }
+            else
+                initEffect(3, Player1.transform.GetChild(3).transform.position);
             ProcessDanceNPC(temp);
 
             switch (sessionResult)
@@ -196,9 +203,6 @@ public class GameManager : MonoBehaviour
 
     void InitPlayer()
     {
-        Player1 = Instantiate(Player1Prefab, posLeft, Quaternion.identity);
-        Player2 = Instantiate(Player2Prefab, posRight, Quaternion.identity);
-        Player3 = Instantiate(Player3Prefab, posMid, Quaternion.identity);
         RefreshPosPlayer();
     }
 }
